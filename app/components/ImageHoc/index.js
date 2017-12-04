2c03bebd60ce967bdc926b36ac8e4c1537ee73eb
Jesse Hill <jsnahill@gmail.com>
2017-12-04 16:55:14 -0600

    Create placeholder with note for image HOC

---
+++ app/components/ImageHoc/index.js
@@ -0,0 +1,32 @@
+/**
+*
+* ImageHoc
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+// do the following to any images wrapped in this component
+// const imgDefer = document.getElementsByTagName('img');
+// const imgArray = [...imgDefer];
+
+// imgArray.forEach((img) => {
+//   if (img.getAttribute('data-src')) {
+//     img.setAttribute('src', img.getAttribute('data-src'));
+//     img.setAttribute('class', 'loaded');
+//   }
+// });
+
+function ImageHoc() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+ImageHoc.propTypes = {
+
+};
+
+export default ImageHoc;
---
+++ app/components/ImageHoc/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ImageHoc from '../index';
+
+describe('<ImageHoc />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
