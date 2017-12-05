381710b1c320985cd504163e8a5bf900606dedc3
Jesse Hill <jsnahill@gmail.com>
2017-12-05 11:10:49 -0600

    Add helper function for sending api requests

---
+++ app/utils/request.js
@@ -0,0 +1,19 @@
+import 'whatwg-fetch';
+
+const parseJSON = (res) => res.json();
+
+const checkStatus = (res) => {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  }
+
+  const error = new Error(res.statusText);
+  error.response = res;
+  throw error;
+};
+
+const request = (url, options) => fetch(url, options)
+  .then(checkStatus)
+  .then(parseJSON);
+
+export default request;
