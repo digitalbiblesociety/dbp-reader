ae23d5950b25f6e2b1f5348f66ad2acf1eead225
Jesse Hill <jsnahill@gmail.com>
2017-12-07 15:49:05 -0600

    Add key to verse divs

--- app/components/Text/index.js
+++ app/components/Text/index.js
@@ -18,7 +18,7 @@ class Text extends React.PureComponent { // eslint-disable-line react/prefer-sta
 			<div className="row small-10 centered main">
 				{
 					text.map((verse) => (
-						<div className="text-container">
+						<div key={verse.verse_start} className="text-container">
 							<h5 className="verse-number">{verse.verse_start}</h5>
 							<h3 className="verse-text">{verse.verse_text}</h3>
 						</div>
