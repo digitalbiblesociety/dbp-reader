375af9b88951da3d9ef92d35709aeb8fd8200f23
Jesse Hill <jsnahill@gmail.com>
2017-12-12 09:32:19 -0700

    Add initial actions to settings container

--- app/containers/Settings/actions.js
+++ app/containers/Settings/actions.js
@@ -6,6 +6,11 @@
 
 import {
   DEFAULT_ACTION,
+	UPDATE_THEME,
+	UPDATE_FONT_TYPE,
+	UPDATE_FONT_SIZE,
+	SAVE_SETTINGS,
+	UNDO_SETTINGS,
 } from './constants';
 
 export function defaultAction() {
@@ -13,3 +18,26 @@ export function defaultAction() {
 		type: DEFAULT_ACTION,
 	};
 }
+
+export const updateTheme = ({ theme }) => ({
+	type: UPDATE_THEME,
+	theme,
+});
+
+export const updateFontType = ({ font }) => ({
+	type: UPDATE_FONT_TYPE,
+	font,
+});
+
+export const updateFontSize = ({ size }) => ({
+	type: UPDATE_FONT_SIZE,
+	size,
+});
+
+export const saveSettings = () => ({
+	type: SAVE_SETTINGS,
+});
+
+export const undoSettings = () => ({
+	type: UNDO_SETTINGS,
+});
--- app/containers/Settings/index.js
+++ app/containers/Settings/index.js
@@ -9,15 +9,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-
 import injectReducer from 'utils/injectReducer';
 import makeSelectSettings from './selectors';
 import reducer from './reducer';
+import {
+	updateTheme,
+	updateFontType,
+	updateFontSize,
+	saveSettings,
+	undoSettings,
+} from './actions';
 // import messages from './messages';
 // import { FormattedMessage } from 'react-intl';
 
 // add icon for settings close button
 export class Settings extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+	updateTheme = ({ theme }) => this.props.dispatch(updateTheme({ theme }));
+	updateFontType = ({ font }) => this.props.dispatch(updateFontType({ font }));
+	updateFontSize = ({ size }) => this.props.dispatch(updateFontSize({ size }));
+	saveSettings = () => this.props.dispatch(saveSettings());
+	undoSettings = () => this.props.dispatch(undoSettings());
+
 	render() {
 		return (
 			<div>
