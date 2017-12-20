47fa086ee34cd0fae9552fbdebc044453940f106
Jesse Hill <jsnahill@gmail.com>
2017-12-20 15:22:37 -0600

    Style profile

--- app/containers/Profile/index.js
+++ app/containers/Profile/index.js
@@ -7,17 +7,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+// import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 import menu from 'images/menu.svg';
+import SvgWrapper from 'components/SvgWrapper';
 import makeSelectProfile from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
+// import messages from './messages';
 
 export class Profile extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 	render() {
@@ -30,15 +32,30 @@ export class Profile extends React.PureComponent { // eslint-disable-line react/
 						<svg className="icon"><use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref={`${menu}#close`}></use></svg>
 					</span>
 				</header>
-				<div>Signing up lets you create Bookmarks, Highlights and Notes, and access them wherever you use Bible.is!</div>
-				<input className="email" />
-				<input className="first-password" />
-				<input className="second-password" />
-				<div className="sign-up-button">SIGN UP</div>
-				<div className="google">Sign up with google</div>
-				<div className="facebook">Sign up with facebook</div>
-				<div className="disclaimer">By creating an account, you agree to the Bible.is<a>Privacy Policy</a>&<a>Terms of Use</a>.</div>
-				<FormattedMessage {...messages.header} />
+				<div className="form-options">
+					<h1 className="login">LOGIN</h1>
+					<h1 className="signup">SIGN UP</h1>
+				</div>
+				<section className="message">
+					<p>Signing up lets you create Bookmarks, Highlights and Notes, and access them wherever you use Bible.is!</p>
+				</section>
+				<input className="email" placeholder="Enter your email" />
+				<input className="first-password" placeholder="Enter a password" />
+				<input className="second-password" placeholder="Re-enter your password" />
+				<div className="sign-up-button"><span className="text">SIGN UP</span></div>
+				<div className="google">
+					<SvgWrapper className="svg" height="30px" width="30px" fill="#fff" svgid="google_plus" />
+					Sign up with Google
+				</div>
+				<div className="facebook">
+					<SvgWrapper className="svg" height="30px" width="30px" fill="#fff" svgid="facebook" />
+					Sign up with Facebook
+				</div>
+				<section className="disclaimer">
+					By creating an account, you agree to the Bible.is
+					<Link className="link" to="/privacy-policy"> Privacy Policy </Link> &
+					<Link className="link" to="/terms-of-use"> Terms of Use</Link>.
+				</section>
 			</aside>
 		);
 	}
