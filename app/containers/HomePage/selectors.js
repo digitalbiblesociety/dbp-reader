5e0d732e456225e3c4df9ea4b9ffa5ddc55a847b
Jesse Hill <jsnahill@gmail.com>
2017-12-28 17:45:42 -0600

    Select Next/Prev book, Available books placeholder
    Added placeholder functions for determining the available
    books based on the selected translation.
    Added selectors for getting the next or previous book.

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+// import bookNames from 'utils/listOfBooksInBible';
 
 /**
  * Direct selector to the homepage state domain
@@ -8,6 +9,49 @@ const selectHomePageDomain = (state) => state.get('homepage');
 /**
  * Other specific selectors
  */
+const selectActiveBook = () => createSelector(
+  selectHomePageDomain,
+  (substate) => {
+	const books = substate.get('books');
+	const activeBookId = substate.get('activeBookId');
+	const activeBook = books.filter((book) => book.get('book_id') === activeBookId).get(0);
+
+	return activeBook;
+}
+);
+
+const selectNextBook = () => createSelector(
+	selectHomePageDomain,
+	(substate) => {
+		const books = substate.get('books');
+		const activeBookId = substate.get('activeBookId');
+		const activeBookIndex = books.findIndex((book) => book.get('book_id') === activeBookId);
+		const nextBook = books.get(activeBookIndex + 1);
+		return nextBook;
+	}
+);
+
+const selectPrevBook = () => createSelector(
+	selectHomePageDomain,
+	(substate) => {
+		const books = substate.get('books');
+		const activeBookId = substate.get('activeBookId');
+		const activeBookIndex = books.findIndex((book) => book.get('book_id') === activeBookId);
+		const previousBook = books.get(activeBookIndex - 1);
+		return previousBook;
+	}
+);
+
+// Most of function needed to determine which books are available for the selected text
+// const selectAvailableBookNames = () => createSelector(
+//   selectHomePageDomain,
+//   (substate) => {
+// 	const books = substate.get('books').filter((book) => bookNames[book.get('book_id')]);
+// 	const bookList = books.map((book) => book.get('book_id'));
+//
+// 	return bookList;
+// }
+// );
 
 /**
  * Default selector used by HomePage
@@ -21,4 +65,7 @@ const makeSelectHomePage = () => createSelector(
 export default makeSelectHomePage;
 export {
   selectHomePageDomain,
+  selectActiveBook,
+	selectNextBook,
+	selectPrevBook,
 };
