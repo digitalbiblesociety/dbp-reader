6787bfe468a31c20a231dcd403fa68b14ef96c99
Jesse Hill <jsnahill@gmail.com>
2017-12-28 14:38:56 -0600

    Add animation and styling to prev/next arrows

--- app/components/Text/index.js
+++ app/components/Text/index.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import SvgWrapper from 'components/SvgWrapper';
 
 class Text extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 	render() {
@@ -14,13 +15,17 @@ class Text extends React.PureComponent { // eslint-disable-line react/prefer-sta
 		} = this.props;
 
 		return (
-			<main className="chapter">
-				{
-					text.map((verse) => (
-						<span key={verse.verse_start}>&nbsp;<sup>{verse.verse_start}</sup>&nbsp;{verse.verse_text}</span>
-					))
-				}
-			</main>
+			<div className="text-container">
+				<SvgWrapper className="prev-arrow-svg" svgid="prev-arrow" />
+				<main className="chapter">
+					{
+						text.map((verse) => (
+							<span key={verse.verse_start}>&nbsp;<sup>{verse.verse_start}</sup>&nbsp;{verse.verse_text}</span>
+						))
+					}
+				</main>
+				<SvgWrapper className="next-arrow-svg" svgid="next-arrow" />
+			</div>
 		);
 	}
 }
