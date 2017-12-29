c6a11db9ab58d977ca96a470431f44a8b6cdb2ae
Jesse Hill <jsnahill@gmail.com>
2017-12-29 15:22:22 -0600

    Work towards capturing highlighted text and such'

--- app/components/Text/index.js
+++ app/components/Text/index.js
@@ -7,8 +7,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SvgWrapper from 'components/SvgWrapper';
-
+/* Disabling the jsx-a11y linting because we need to capture the selected text
+	 and the most straight forward way of doing so is with the onMouseUp event */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 class Text extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+	handleRightClick = () => {
+		// Can potentially use the below menu to activate the menu for note taking
+		// if (e.button === 2) {}
+		// Below code gets the highlighted text
+		// window.getSelection().toString();
+	}
+	// Allows us to use the right mouse button to toggle menu's or perform different actions
+	handleContext = (e) => e.preventDefault()
+
 	render() {
 		const {
 			text,
@@ -25,7 +36,7 @@ class Text extends React.PureComponent { // eslint-disable-line react/prefer-sta
 						<SvgWrapper onClick={prevChapter} className="prev-arrow-svg" svgid="prev-arrow" />
 					)
 				}
-				<main className="chapter">
+				<main onMouseUp={this.handleRightClick} className="chapter" onContextMenu={this.handleContext}>
 					{
 						text.map((verse) => (
 							<span key={verse.verse_start}>&nbsp;<sup>{verse.verse_start}</sup>&nbsp;{verse.verse_text}</span>
