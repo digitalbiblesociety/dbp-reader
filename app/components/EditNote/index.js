7e4c146f28c0309154d3debfd211524911467150
Jesse Hill <jsnahill@gmail.com>
2018-01-02 14:46:20 -0600

    Build skeleton for edit notes menu

--- app/components/EditNote/index.js
+++ app/components/EditNote/index.js
@@ -5,17 +5,27 @@
 */
 
 import React from 'react';
+import SvgWrapper from 'components/SvgWrapper';
 // import styled from 'styled-components';
-
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 class EditNote extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 	render() {
 		return (
-			<div>
-				<FormattedMessage {...messages.header} />
-			</div>
+			<section className="edit-notes">
+				<div className="date-title">
+					<span className="date">01.01.18</span>
+					<span className="title">ADD TITLE</span>
+				</div>
+				<div className="verse-dropdown">
+					<SvgWrapper className="svg" height="20px" width="20px" svgid="go-right" />
+					<span className="text">GENESIS 12:13</span>
+					<span className="version-dropdown">ENGESV</span></div>
+				<div className="add-verse">
+					<SvgWrapper className="plus" width="20px" height="20px" svgid="plus" />
+					<span className="text">ADD VERSE</span>
+				</div>
+				<div className="note-text">CLICK TO ENTER TEXT</div>
+			</section>
 		);
 	}
 }
--- app/components/MyNotes/index.js
+++ app/components/MyNotes/index.js
@@ -7,9 +7,6 @@
 import React from 'react';
 // import styled from 'styled-components';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
-
 class MyNotes extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 	render() {
 		return (
@@ -23,7 +20,6 @@ class MyNotes extends React.PureComponent { // eslint-disable-line react/prefer-
 					<span>1</span>
 					<input type="dropdown" placeholder="10 PER PAGE" />
 				</div>
-				<FormattedMessage {...messages.header} />
 			</div>
 		);
 	}
