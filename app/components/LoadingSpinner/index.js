fc0db4b411c24d5057c3eccfb5f2466e63bff823
Jesse Hill <jsnahill@gmail.com>
2018-01-25 09:55:31 -0600

    Make loading spinner

---
+++ app/components/LoadingSpinner/index.js
@@ -0,0 +1,32 @@
+/**
+*
+* LoadingSpinner
+*
+*/
+
+import React from 'react';
+
+function LoadingSpinner() {
+	return (
+		<div className="loading-spinner">
+			<span className="loading-spinner1 loading-spinner-child" />
+			<span className="loading-spinner2 loading-spinner-child" />
+			<span className="loading-spinner3 loading-spinner-child" />
+			<span className="loading-spinner4 loading-spinner-child" />
+			<span className="loading-spinner5 loading-spinner-child" />
+			<span className="loading-spinner6 loading-spinner-child" />
+			<span className="loading-spinner7 loading-spinner-child" />
+			<span className="loading-spinner8 loading-spinner-child" />
+			<span className="loading-spinner9 loading-spinner-child" />
+			<span className="loading-spinner10 loading-spinner-child" />
+			<span className="loading-spinner11 loading-spinner-child" />
+			<span className="loading-spinner12 loading-spinner-child" />
+		</div>
+	);
+}
+
+LoadingSpinner.propTypes = {
+
+};
+
+export default LoadingSpinner;
---
+++ app/components/LoadingSpinner/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import LoadingSpinner from '../index';
+
+describe('<LoadingSpinner />', () => {
+	it('Expect to have unit tests specified', () => {
+		expect(true).toEqual(false);
+	});
+});
