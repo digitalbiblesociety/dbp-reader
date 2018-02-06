bf9d18934b73e9424f2e66753dc8fbce0101a1d7
Jesse Hill <jsnahill@gmail.com>
2018-02-06 14:51:17 -0600

    More work on footnotes
    Defaulted footnotes to just sit at the bottom of the page. Having issue implementing a solution for creating popups.
    Going to probably need to resort to directly manipulating the dom for this functionality.

--- app/containers/Text/htmlToReact.js
+++ app/containers/Text/htmlToReact.js
@@ -17,21 +17,48 @@ export const addClickToNotes = ({ html, action }) => {
 	const processingInstructions = [
 		{
 			// Custom <h1> processing
-			replaceChildren: true,
+			// replaceChildren: true,
 			shouldProcessNode(node) {
 				if (node.attribs && node.attribs.class === 'note' && node.attribs.id === 'note-0') {
-					return node.attribs.class === 'note';
+					return node.attribs.class === 'note' && node.attribs.id.includes('note');
 				}
 				return false;
 				// console.log('in second should process', node)
 				// return node.parent && node.parent.name && node.parent.name === 'h1';
 			},
-			processNode(node, children) {
+			processNode(node, children, index) {
 				const footnoteId = children[0].props.href.slice(1);
-				const newNode = { ...node, attribs: { ...node.attribs, onClick: () => action({ footnoteId, coords: { x: 5, y: 15 } }) } };
-				// console.log('old node', node);
-				console.log('new node', newNode);
-				return React.createElement(newNode);
+				const content = children[0].props.children;
+				// console.log(children[0])
+				// const newNode = node;
+				// console.log(node)
+				// newNode.attribs.onClick = () => action({ footnoteId, coords: { x: 5, y: 15 } });
+				// // const newNode = { ...node, attribs: { ...node.attribs, onClick: () => action({ footnoteId, coords: { x: 5, y: 15 } }) } };
+				// // console.log('old node', node);
+				// console.log('new element', React.createElement(newNode));
+				// return React.createElement(newNode);
+				// node.attribs.onClick = () => action({ footnoteId, coords: { x: 5, y: 15 } })
+				// console.log(node.attribs.onClick)
+				// return node.attribs.onClick;
+				const element = {
+					$$typeof: Symbol.for('react.element'),
+					type: 'span',
+					props: {
+						onClick: () => action({ footnoteId, coords: { x: 5, y: 15 } }),
+						className: 'note',
+						key: index,
+					},
+					children: [
+						{
+							$$typeof: Symbol.for('react.element'),
+							type: 'text',
+							children: content,
+						},
+					],
+				};
+				// console.log(element);
+				// console.log(React.createElement(element.type, element.props, element.children))
+				return React.createElement(element.type, element.props, [element.children]);
 				// node.attribs.onClick = () => action({ footnoteId, coords: { x: 5, y: 15 } });
 				// return node;
 			},
@@ -46,6 +73,6 @@ export const addClickToNotes = ({ html, action }) => {
 	const reactComponents = htmlToReactParser.parseWithInstructions(html, isValidNode,
 		processingInstructions);
 	previousResult = reactComponents[0];
-	// console.log('React component', reactComponents[0]);
+
 	return reactComponents[0];
 };
