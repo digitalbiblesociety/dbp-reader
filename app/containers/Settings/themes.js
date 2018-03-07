21eb0f1563e715ee77e22e424d088744c13e7049
Jesse Hill <jsnahill@gmail.com>
2018-03-07 11:23:59 -0600

    Updates to the theme colors and settings menu styles

--- app/containers/Settings/themes.js
+++ app/containers/Settings/themes.js
@@ -10,13 +10,15 @@ const paper = {
 	'--menu-secondary-header-background': 'rgb(170, 134, 119)',
 	'--menu-background-color': 'rgb(101, 55, 45)',
 	'--menu-secondary-background-color': 'rgb(248, 228, 211)',
-	'--menu-title-text-color': 'rgb(105, 59, 49)',
+	'--menu-title-text-color': 'rgb(255, 255, 255)',
+	'--menu-active-text-color': 'rgb(112, 63, 51)',
 	'--menu-active-color': 'rgb(98, 177, 130)',
 	'--menu-hover-color': 'rgb(98, 177, 130)',
 	'--menu-font-color': 'rgb(134, 97, 87)',
 	'--menu-inactive-svg-color': 'rgb(61,65,68)',
 	'--application-background': 'rgb(253, 248, 241)',
 	'--application-base-color': 'rgb(110, 67, 57)',
+	'--settings-option-inactive-secondary-color': 'rgb(255, 255, 255)',
 };
 
 const dark = {
@@ -29,7 +31,8 @@ const dark = {
 	'--settings-font-color': 'rgb(99, 105, 106)',
 	'--menu-font-color': 'rgb(255, 255, 255)',
 	'--menu-inactive-svg-color': 'rgb(61,65,68)',
-	'--menu-background-color': 'rgb(39, 42, 47)',
+	'--menu-background-color': 'rgb(44, 48, 54)',
+	'--menu-active-text-color': 'rgb(255, 255, 255)',
 	'--menu-secondary-background-color': 'rgb(39, 42, 47)',
 	'--menu-hover-color': 'rgb(98, 177, 130)',
 	'--menu-active-color': 'rgb(98, 177, 130)',
@@ -38,6 +41,7 @@ const dark = {
 	'--menu-header-background': 'rgb(26, 29, 33)',
 	'--application-background': 'rgb(26, 29, 33)',
 	'--application-base-color': 'rgb(255, 255, 255)',
+	'--settings-option-inactive-secondary-color': 'rgb(27, 31, 46)',
 };
 
 const red = {
@@ -50,7 +54,8 @@ const red = {
 	'--settings-font-color': 'rgb(99, 105, 116)',
 	'--menu-font-color': 'rgb(255, 255, 255)',
 	'--menu-inactive-svg-color': 'rgb(61,65,68)',
-	'--menu-background-color': 'rgb(39, 42, 47)',
+	'--menu-active-text-color': 'rgb(255, 255, 255)',
+	'--menu-background-color': 'rgb(44, 48, 54)',
 	'--menu-secondary-background-color': 'rgb(39, 42, 47)',
 	'--menu-hover-color': 'rgb(98, 177, 130)',
 	'--menu-active-color': 'rgb(98, 177, 130)',
@@ -59,6 +64,7 @@ const red = {
 	'--menu-header-background': 'rgb(115, 16, 37)',
 	'--application-background': 'rgb(253, 248, 241)',
 	'--application-base-color': 'rgb(50, 53, 58)',
+	'--settings-option-inactive-secondary-color': 'rgb(27, 31, 46)',
 };
 
 const themes = {
