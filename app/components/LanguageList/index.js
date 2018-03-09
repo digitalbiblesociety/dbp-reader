608d56b12c88a688d90698a54921183c685b641e
Jesse Hill <jsnahill@gmail.com>
2018-03-09 14:33:34 -0600

    Implement lazy loading for the languages list

--- app/components/LanguageList/index.js
+++ app/components/LanguageList/index.js
@@ -6,6 +6,8 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+// import LazyLoad, { forceCheck } from 'react-lazyload';
+import { List, AutoSizer } from 'react-virtualized';
 import SvgWrapper from 'components/SvgWrapper';
 import LoadingSpinner from 'components/LoadingSpinner';
 
@@ -14,10 +16,11 @@ class LanguageList extends React.PureComponent { // eslint-disable-line react/pr
 		super(props);
 		this.state = {
 			filterText: '',
+
 		};
 	}
-
-	get filteredLanguages() {
+	// Try to reduce the number of times language list is iterated over
+	getFilteredLanguages(width, height) {
 		const {
 			languages,
 			activeLanguageName,
@@ -25,17 +28,55 @@ class LanguageList extends React.PureComponent { // eslint-disable-line react/pr
 		const { filterText } = this.state;
 		const filteredLanguages = filterText ? languages.filter((language) => this.filterFunction(language, filterText)) : languages;
 
-		const components = filteredLanguages.map((language) => (
-			<div className="language-name" key={language.get('iso_code')} role="button" tabIndex={0} onClick={() => this.handleLanguageClick(language)}>
-				<h4 className={language.get('name') === activeLanguageName ? 'active-language-name' : ''}>{language.get('name')}</h4>
-			</div>
-		));
+		// const components = () => filteredLanguages.map((language) => (
+		// 	<div className="language-name" key={language.get('iso_code')} role="button" tabIndex={0} onClick={() => this.handleLanguageClick(language)}>
+		// 		<h4 className={language.get('name') === activeLanguageName ? 'active-language-name' : ''}>{language.get('name')}</h4>
+		// 	</div>
+		// ));
 
 		if (languages.size === 0) {
 			return <span>There was an error fetching this resource, an Admin has been notified. We apologize for the inconvenience</span>;
 		}
 
-		return components.size ? components : <span>There are no matches for your search.</span>;
+		const renderARow = ({ index, style, key }) => {
+			const language = filteredLanguages.get(index);
+			// key={language.get('iso_code')}
+			// if (isScrolling) {
+			// 	return <div key={key} style={style}>scrolling...</div>;
+			// }
+			return (
+				<div style={style} key={key} className="language-name" role="button" tabIndex={0} onClick={() => this.handleLanguageClick(language)}>
+					<h4 className={language.get('name') === activeLanguageName ? 'active-language-name' : ''}>{language.get('name')}</h4>
+				</div>
+			);
+		};
+
+		const getActiveIndex = () => {
+			let activeIndex = 0;
+
+			filteredLanguages.forEach((l, i) => {
+				if (l.get('name') === activeLanguageName) {
+					activeIndex = i;
+				}
+			});
+
+			return activeIndex;
+		};
+
+		return filteredLanguages.size ? (
+			<List
+				estimatedRowSize={28 * filteredLanguages.size}
+				height={height}
+				rowRenderer={renderARow}
+				rowCount={filteredLanguages.size}
+				overscanRowCount={10}
+				rowHeight={28}
+				scrollToIndex={getActiveIndex()}
+				width={width}
+			/>
+		) : <span>There are no matches for your search.</span>;
+
+		// return components.size ? components : <span>There are no matches for your search.</span>;
 	}
 
 	filterFunction = (language, filterText) => {
@@ -100,9 +141,11 @@ class LanguageList extends React.PureComponent { // eslint-disable-line react/pr
 					</span>
 					<div className="language-name-list">
 						{
-							loadingLanguages ? (
-								<LoadingSpinner />
-							) : this.filteredLanguages
+							!loadingLanguages ? (
+								<AutoSizer>
+									{({ width, height }) => this.getFilteredLanguages(width, height)}
+								</AutoSizer>
+							) : <LoadingSpinner />
 						}
 					</div>
 				</div>
