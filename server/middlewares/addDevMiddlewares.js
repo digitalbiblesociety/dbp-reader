4ec1e0217f54be07d80bd0702092242e4d922862
Jesse Hill <jsnahill@gmail.com>
2018-03-14 09:50:51 -0500

    Replace spaces with tabs

--- server/middlewares/addDevMiddlewares.js
+++ server/middlewares/addDevMiddlewares.js
@@ -4,32 +4,32 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 function createWebpackMiddleware(compiler, publicPath) {
-  return webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath,
-    silent: true,
-    stats: 'errors-only',
-  });
+	return webpackDevMiddleware(compiler, {
+		noInfo: true,
+		publicPath,
+		silent: true,
+		stats: 'errors-only',
+	});
 }
 
 module.exports = function addDevMiddlewares(app, webpackConfig) {
-  const compiler = webpack(webpackConfig);
-  const middleware = createWebpackMiddleware(compiler, webpackConfig.output.publicPath);
+	const compiler = webpack(webpackConfig);
+	const middleware = createWebpackMiddleware(compiler, webpackConfig.output.publicPath);
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
+	app.use(middleware);
+	app.use(webpackHotMiddleware(compiler));
 
   // Since webpackDevMiddleware uses memory-fs internally to store build
   // artifacts, we use it instead
-  const fs = middleware.fileSystem;
+	const fs = middleware.fileSystem;
 
-  app.get('*', (req, res) => {
-    fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
-      if (err) {
-        res.sendStatus(404);
-      } else {
-        res.send(file.toString());
-      }
-    });
-  });
+	app.get('*', (req, res) => {
+		fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
+			if (err) {
+				res.sendStatus(404);
+			} else {
+				res.send(file.toString());
+			}
+		});
+	});
 };
