9b69999b6ff00dfbc276f8e21d961a0fbd947291
Jesse Hill <jsnahill@gmail.com>
2018-04-04 11:35:59 -0500

    Work on sign up components

--- app/components/SignUp/index.js
+++ app/components/SignUp/index.js
@@ -8,7 +8,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FacebookAuthentication from 'containers/FacebookAuthentication';
 import GoogleAuthentication from 'containers/GoogleAuthentication';
-import SvgWrapper from 'components/SvgWrapper';
+// import SvgWrapper from 'components/SvgWrapper';
 // import styled from 'styled-components';
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
@@ -20,9 +20,11 @@ class SignUp extends React.PureComponent {
 		email: '',
 		firstName: '',
 		lastName: '',
-		validPassword: false,
-		validEmail: false,
+		passwordErrorType: '',
+		validPassword: true,
+		validEmail: true,
 		wasSignupSent: false,
+		showSignupError: false,
 	}
 
 	componentWillReceiveProps(nextProps) {
@@ -33,53 +35,60 @@ class SignUp extends React.PureComponent {
 		}
 	}
 
-	componentDidUpdate(prevProps, prevState) {
-		if ((prevState.confirmPassword !== prevState.password) && (this.state.password === this.state.confirmPassword)) {
-			this.setValidPassword(true);
-		} else if (((prevState.confirmPassword === prevState.password) && prevState.password.length) && (this.state.password !== this.state.confirmPassword)) {
-			this.setValidPassword(false);
-		}
+	// componentDidUpdate(prevProps, prevState) {
+	// 	if ((prevState.confirmPassword !== prevState.password) && this.checkValidPassword()) {
+	// 		console.log('valid')
+	// 		this.setValidPassword(true);
+	// 	} else if (((prevState.confirmPassword === prevState.password) && prevState.password.length) && !this.checkValidPassword()) {
+	// 		this.setValidPassword(false);
+	// 	}
+	// }
+
+	componentWillUnmount() {
+		this.props.viewErrorMessage();
 	}
 
-	setValidPassword = (valid) => {
-		this.setState({ validPassword: valid });
-	}
+	// setValidPassword = (valid) => {
+	// 	this.setState({ validPassword: valid });
+	// 	this.viewError();
+	// }
 
 	handlePasswordChange = (e) => {
 		this.setState({ password: e.target.value });
+		this.viewError();
 	}
 
 	handleFirstName = (e) => {
 		this.setState({ firstName: e.target.value });
+		this.viewError();
 	}
 
 	handleLastName = (e) => {
 		this.setState({ lastName: e.target.value });
+		this.viewError();
 	}
 
 	handleEmailChange = (e) => {
 		const email = e.target.value;
-		const indexOfAt = email.indexOf('@');
 
-		if (indexOfAt !== -1) {
-			this.setState({
-				validEmail: true,
-				email,
-			});
-		} else {
-			this.setState({ email, validEmail: false });
-		}
+		this.setState({ email });
+		this.viewError();
 	}
 
 	handleConfirmPassword = (e) => {
 		const value = e.target.value;
 
 		this.setState({ confirmPassword: value });
+		this.viewError();
 	}
 
 	handleSignup = (e) => {
 		e.preventDefault();
-		if (this.state.validPassword && this.state.validEmail) {
+		const validEmail = this.checkValidEmail();
+		const validPassword = this.checkValidPassword();
+
+		if (validPassword && validEmail) {
+			// console.log('sending sign up form');
 			this.props.sendSignupForm({
 				email: this.state.email,
 				password: this.state.password,
@@ -88,20 +97,86 @@ class SignUp extends React.PureComponent {
 				lastName: this.state.lastName,
 			});
 			this.setState({ wasSignupSent: true });
+		} else if (!validPassword) {
+			// password error message
+			this.setState({ showSignupError: true });
+			// console.log('password error');
+		} else if (!validEmail) {
+			// email error message
+			this.setState({ showSignupError: true });
+			// console.log('email error');
 		}
+		this.setState({ validPassword });
+		this.setState({ validEmail });
+		this.viewError();
+	}
+
+	checkValidEmail = () => {
+		const { email } = this.state;
+		const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email); // eslint-disable-line no-useless-escape
+		// console.log('is valid email', validEmail);
+
+		return validEmail;
+	}
+
+	checkValidPassword = () => {
+		const { confirmPassword, password } = this.state;
+		const passLength = password.length >= 8;
+		const passEqual = password === confirmPassword;
+		const passNotPass = password !== 'password';
+		const upperNumSym = /(?=.*\d)|(?=.*[A-Z])|(?=.*[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/])/.test(password); // eslint-disable-line no-useless-escape
+		// console.log('upper num sym', upperNumSym);
+		const validPassword = passLength && passEqual && passNotPass && upperNumSym;
+
+		if (!passEqual) {
+			this.setState({ passwordErrorType: 'confirm' });
+		} else if (!passLength) {
+			this.setState({ passwordErrorType: 'length' });
+		} else if (!passNotPass) {
+			this.setState({ passwordErrorType: 'password' });
+		} else if (!upperNumSym) {
+			this.setState({ passwordErrorType: 'upperNumSym' });
+		}
+		// console.log('valid password', validPassword);
+
+		if (validPassword) {
+			this.setState({ passwordErrorType: '' });
+		}
+
+		return validPassword;
+	}
+
+	get signupError() {
+		const { validEmail, passwordErrorType } = this.state;
+
+		if (passwordErrorType) {
+			if (passwordErrorType === 'confirm') {
+				return <span className={'signup-error-message'}>Your passwords do not match.</span>;
+			} else if (passwordErrorType === 'length') {
+				return <span className={'signup-error-message'}>Your password must be longer than 8 characters.</span>;
+			} else if (passwordErrorType === 'password') {
+				return <span className={'signup-error-message'}>You cannot use, &quot;password&quot; as your password...</span>;
+			} else if (passwordErrorType === 'upperNumSym') {
+				return <span className={'signup-error-message'}>You must have at least one of the following: number, uppercase character, symbol.</span>;
+			}
+		}
+
+		if (!validEmail) {
+			return <span className={'signup-error-message'}>You have entered an invalid email.</span>;
+		}
+
+		return <span className={'signup-error-message'}>There was an unknown error, please try again.</span>;
 	}
 
 	get signupForm() {
+		const { validEmail, validPassword } = this.state;
 		return (
 			<form onSubmit={this.handleSignup}>
-				<span className={'input-wrapper'}>
-					<SvgWrapper className="svg" width="30px" height="30px" fill="#fff" svgid="e-mail" />
-					<input autoComplete={'email'} onChange={this.handleEmailChange} className="email" placeholder="Enter E-mail" value={this.state.email} />
-				</span>
-				<input autoComplete={'given-name'} onChange={this.handleFirstName} className="name-inputs" placeholder="Enter First Name" value={this.state.firstName} />
-				<input autoComplete={'family-name'} onChange={this.handleLastName} className="name-inputs" placeholder="Enter Last Name" value={this.state.lastName} />
-				<input autoComplete="new-password" type="password" onChange={this.handlePasswordChange} className="first-password" placeholder="Enter Password" value={this.state.password} />
-				<input autoComplete="new-password" type="password" onChange={this.handleConfirmPassword} className="second-password" placeholder="Confirm Password" value={this.state.confirmPassword} />
+				<input autoComplete={'email'} onChange={this.handleEmailChange} className={validEmail ? 'email' : 'email error'} placeholder="E-mail" value={this.state.email} />
+				<input autoComplete={'given-name'} onChange={this.handleFirstName} className={'name-inputs'} placeholder="Given Name" value={this.state.firstName} />
+				<input autoComplete={'family-name'} onChange={this.handleLastName} className={'name-inputs'} placeholder="Family Name" value={this.state.lastName} />
+				<input autoComplete="new-password" type="password" onChange={this.handlePasswordChange} className={validPassword ? 'first-password' : 'first-password error'} placeholder="Password" value={this.state.password} />
+				<input autoComplete="new-password" type="password" onChange={this.handleConfirmPassword} className={validPassword ? 'second-password' : 'second-password error'} placeholder="Confirm Password" value={this.state.confirmPassword} />
 				<div className="sign-up-button">
 					<button type="submit" className="text">SIGN UP</button>
 				</div>
@@ -109,25 +184,38 @@ class SignUp extends React.PureComponent {
 		);
 	}
 
+	viewError = () => {
+		if (!this.props.errorMessageViewed) {
+			this.props.viewErrorMessage();
+		}
+		if (this.state.showSignupError) {
+			this.setState({ showSignupError: false });
+		}
+	}
+
 	render() {
 		const {
-			errorMessage,
+			errorMessageViewed,
 			socialLoginLink,
 			socialMediaLogin,
 			activeDriver,
 		} = this.props;
+		const { showSignupError } = this.state;
 
 		return (
 			<React.Fragment>
 				<section className="message">
 					<p>Signing up lets you create Bookmarks, Highlights and Notes, and access them wherever you use Bible.is!</p>
 				</section>
+				{this.signupForm}
 				{
-					errorMessage ? (
-						<div className="signup-error-message">{errorMessage}</div>
+					showSignupError ? this.signupError : null
+				}
+				{
+					!errorMessageViewed ? (
+						<div className="signup-error-message">This email is already registered with an account. Please try a different email or sign in.</div>
 					) : null
 				}
-				{this.signupForm}
 				<GoogleAuthentication activeDriver={activeDriver} socialMediaLogin={socialMediaLogin} socialLoginLink={socialLoginLink} />
 				<FacebookAuthentication activeDriver={activeDriver} socialMediaLogin={socialMediaLogin} socialLoginLink={socialLoginLink} />
 				<section className="disclaimer">
@@ -143,9 +231,11 @@ class SignUp extends React.PureComponent {
 SignUp.propTypes = {
 	sendSignupForm: PropTypes.func,
 	socialMediaLogin: PropTypes.func,
-	socialLoginLink: PropTypes.string,
-	errorMessage: PropTypes.string,
+	viewErrorMessage: PropTypes.func,
+	// errorMessage: PropTypes.string,
 	activeDriver: PropTypes.string,
+	socialLoginLink: PropTypes.string,
+	errorMessageViewed: PropTypes.bool,
 };
 
 export default SignUp;
