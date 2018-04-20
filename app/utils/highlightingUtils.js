cbf5dc7adecf9549f1aac81f745ab30f4a7455b0
Jesse Hill <jsnahill@gmail.com>
2018-04-20 16:36:02 -0500

    Fix issue with a highlight overlapping a footnote

---
+++ app/utils/highlightingUtils.js
@@ -0,0 +1,82 @@
+const getFormattedParentVerseNumber = (node, verseNumber) => {
+	// Require both parameters
+	if (!node || !verseNumber) {
+		return null;
+	}
+
+	// set counter to prevent any possibility of an infinite loop
+	let counter = 0;
+	let newNode = node;
+
+	while (!(newNode.attributes && newNode.attributes['data-id'] && newNode.attributes['data-id'].value.split('_')[1] !== verseNumber)) {
+		newNode = newNode.parentNode;
+		if (counter >= 10) break;
+		counter += 1;
+		// console.log('condition to be checked', !(node.attributes && node.attributes['data-id'] && node.attributes['data-id'].value.split('_')[1] !== firstVerse));
+	}
+
+	return newNode;
+};
+
+const getFormattedParentVerse = (node) => {
+	// Require both parameters
+	if (!node) {
+		return null;
+	}
+
+	// set counter to prevent any possibility of an infinite loop
+	let counter = 0;
+	let newNode = node;
+	// Finds the first parent that has a data-id and a class of .v
+	// Both of these things exist only on a verse element
+	while (!(newNode.attributes && newNode.attributes['data-id'] && newNode.classList && newNode.classList[0] === 'v')) {
+		newNode = newNode.parentNode;
+		if (counter >= 10) break;
+		counter += 1;
+		// console.log('condition to be checked', !(node.attributes && node.attributes['data-id'] && node.attributes['data-id'].value.split('_')[1] !== firstVerse));
+	}
+
+	return newNode;
+};
+
+const getFormattedChildIndex = (parent, child) => {
+	if (!parent || !child) {
+		return null;
+	}
+	// default to -1 to simulate default indexOf
+	let childIndex = -1;
+
+	[...parent.childNodes].forEach((node, i) => {
+		if (node.isSameNode(child) || node.contains(child)) {
+			childIndex = i;
+		}
+	});
+
+	return childIndex;
+};
+
+const getPlainParentVerse = (node, verseNumber) => {
+	// Require both parameters
+	if (!node || !verseNumber) {
+		return null;
+	}
+	// set counter to prevent any possibility of an infinite loop
+	let counter = 0;
+	let newNode = node;
+
+	while (!(newNode.attributes && newNode.attributes.verseid && newNode.attributes.verseid.value !== verseNumber)) {
+		// console.log('newNode', newNode);
+		newNode = newNode.parentNode;
+		if (counter >= 10) break;
+		counter += 1;
+	}
+
+	return newNode;
+};
+
+export {
+	getFormattedParentVerseNumber,
+	getFormattedParentVerse,
+	getPlainParentVerse,
+	getFormattedChildIndex,
+};
