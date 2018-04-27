e8b84450ef2417d4a3be4d55559ff6fadc81bcab
Jesse Hill <jsnahill@gmail.com>
2018-04-27 18:13:34 -0500

    Configure testing suite for testing react components and their functions

--- app/containers/Text/tests/index.test.js
+++ app/containers/Text/tests/index.test.js
@@ -1,18 +1,52 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Text from 'containers/Text';
+import { fromJS } from 'immutable';
 
-import Text from '../index';
+const textProps = {
+	text: [], // array
+	userNotes: [], // array
+	bookmarks: [], // array
+	highlights: [], // array
+	userSettings: fromJS({}), // object
+	formattedSource: fromJS({}), // object
+	nextChapter: () => {}, // func
+	prevChapter: () => {}, // func
+	addBookmark: () => {}, // func
+	addHighlight: () => {}, // func
+	goToFullChapter: () => {}, // func
+	toggleNotesModal: () => {}, // func
+	setActiveNotesView: () => {}, // func
+	toggleInformationModal: () => {}, // func
+	setActiveNote: () => {}, // func
+	activeChapter: 1, // number
+	notesActive: true, // bool
+	invalidBibleId: true, // bool
+	informationActive: true, // bool
+	userAuthenticated: true, // bool
+	loadingNewChapterText: true, // bool
+	userId: '', // string
+	bibleId: '', // string
+	verseNumber: '', // string
+	activeBookId: '', // string
+	activeBookName: '', // string
+};
 
 describe('<Text />', () => {
 	it('Expect to have unit tests specified', () => {
 		expect(true).toEqual(true);
 	});
 	describe('addHighlight', () => {
+		let wrapper;
+
+		beforeEach(() => {
+			wrapper = shallow(<Text {...textProps} />);
+		});
 		it('Should be defined', () => {
-			expect(Text.addHighlight).toBeDefined();
+			expect(wrapper.instance().addHighlight).toBeDefined();
 		});
 		it('Should be a function', () => {
-			expect(typeof Text.addHighlight === 'function').toEqual(true);
+			expect(typeof wrapper.instance().addHighlight === 'function').toEqual(true);
 		});
 	});
 });
--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,3 +1,10 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import 'babel-polyfill';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { XMLSerializer } from 'xmldom';
+
+configure({ adapter: new Adapter() });
+// Add shim for xmlSerializer here so it gets loaded
+global.XMLSerializer = XMLSerializer;
