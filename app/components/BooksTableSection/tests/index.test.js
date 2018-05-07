e3634b40f6a736df80daec3380cc620ade516d69
Jesse Hill <jsnahill@gmail.com>
2018-05-07 11:46:07 -0500

    Build accordion for the book table

---
+++ app/components/BooksTableSection/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import BooksTableSection from '../index';
+
+describe('<BooksTableSection />', () => {
+	it('Expect to have unit tests specified', () => {
+		expect(true).toEqual(false);
+	});
+});
