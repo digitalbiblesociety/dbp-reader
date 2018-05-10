dba0746572cb7602a472a2c5e4552c134947a34e
Jesse Hill <jsnahill@gmail.com>
2018-05-10 13:43:03 -0500

    Control how many pages are displayed at one time for the pagination component

--- app/components/Pagination/index.js
+++ app/components/Pagination/index.js
@@ -9,17 +9,43 @@ import PropTypes from 'prop-types';
 import range from 'lodash/range';
 // import styled from 'styled-components';
 // Doing the number of total pages + 1 because I do not want the list to be 0 indexed
-const Pagination = ({ totalPages, activePage, onChangePage }) => (
-	<div className="item-list">
-		{
-			range(1, totalPages + 1).map((page) => (
-				<div key={page} className={activePage === page ? 'item active' : 'item'}>
-					<span role="button" tabIndex={0} onClick={() => onChangePage(page)}>{page}</span>
-				</div>
-			))
-		}
-	</div>
-);
+// I only want the 4 pages before the active page and the 5 after the active page
+const Pagination = ({ totalPages, activePage, onChangePage }) => {
+	const pageRange = range(1, totalPages + 1);
+	const rangeLength = pageRange.length;
+	const maxPage = activePage + 4 > rangeLength ? rangeLength : activePage + 4;
+	const minPage = activePage - 5 < 0 ? 0 : activePage - 5;
+	const checkMax = maxPage === rangeLength;
+	const checkMin = minPage === 0;
+
+	let visiblePages = pageRange;
+
+	if (checkMax && !checkMin) {
+		visiblePages = rangeLength > 10 ?
+			pageRange.slice(maxPage - 9, maxPage) :
+			pageRange;
+	} else if (checkMin && !checkMax) {
+		visiblePages = rangeLength > 10 ?
+			pageRange.slice(minPage, minPage + 9) :
+			pageRange;
+	} else {
+		visiblePages = rangeLength > 10 ?
+			pageRange.slice(minPage, maxPage) :
+			pageRange;
+	}
+
+	return (
+		<div className="item-list">
+			{
+				visiblePages.map((page) => (
+					<div key={page} className={activePage === page ? 'item active' : 'item'}>
+						<span role="button" tabIndex={0} onClick={() => onChangePage(page)}>{page}</span>
+					</div>
+				))
+			}
+		</div>
+	);
+};
 
 Pagination.propTypes = {
 	onChangePage: PropTypes.func.isRequired,
