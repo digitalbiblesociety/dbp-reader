7f0584fd5cd21444b6b22d79f7855cc497220c27
Jesse Hill <jsnahill@gmail.com>
2018-05-14 15:26:24 -0500

    Add new test case for highlighting plain text

--- app/containers/Text/tests/highlightPlain.test.js
+++ app/containers/Text/tests/highlightPlain.test.js
@@ -252,4 +252,47 @@ describe('highlightPlainText', () => {
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
+	it('Case 8: It should apply a newer highlight over the top of two pre-existing highlights', () => {
+		// const sampleHighlights = [
+		// 	{
+		// 		id: 262,
+		// 		bible_id: 'ENGESV',
+		// 		book_id: 'PSA',
+		// 		chapter: 18,
+		// 		verse_start: 12,
+		// 		highlight_start: 0,
+		// 		highlighted_words: 21,
+		// 		highlighted_color: '#1AF',
+		// 	},
+		// 	{
+		// 		id: 263,
+		// 		bible_id: 'ENGESV',
+		// 		book_id: 'PSA',
+		// 		chapter: 18,
+		// 		verse_start: 12,
+		// 		highlight_start: 11,
+		// 		highlighted_words: 10,
+		// 		highlighted_color: '#5B4',
+		// 	},
+		// ];
+		// const sampleText = chapterText.data.slice(11, 12);
+		// const result = [
+		// 	{
+		// 		book_id: 'PSA',
+		// 		book_name: 'Psalm',
+		// 		book_name_alt: 'Psalm',
+		// 		chapter: 18,
+		// 		chapter_alt: '18',
+		// 		verse_start: 12,
+		// 		verse_start_alt: '12',
+		// 		verse_end: 12,
+		// 		verse_end_alt: '12',
+		// 		hasHighlight: true,
+		// 		verse_text: `<em class="text-highlighted" style="background:${sampleHighlights[0].highlighted_color}">Out of the </em><em class="text-highlighted" style="background:${sampleHighlights[1].highlighted_color}">brightness</em> before him hailstones and coals of fire broke through his clouds.`,
+		// 	},
+		// ];
+
+		// expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
+		expect(false).toBe(true);
+	});
 });
