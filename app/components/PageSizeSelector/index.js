16738406759abc390b4fe4db6921f2e3a88b54bf
Jesse Hill <jsnahill@gmail.com>
2018-05-15 13:02:26 -0500

    Fix issue with the page size selector for the pagination

--- app/components/PageSizeSelector/index.js
+++ app/components/PageSizeSelector/index.js
@@ -8,54 +8,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SvgWrapper from 'components/SvgWrapper';
 // import styled from 'styled-components';
-
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
 
 function PageSizeSelector({ pageSize, togglePageSelector, setPageSize, pageSelectorState: state }) {
 	return (
 		<div className="page-size-selector">
-			{
-				state ? (
-					<div className="open">
-						<div className={pageSize === 10 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(10); togglePageSelector(); }}>
-							<span className={'text'}>10 PER PAGE</span>
-							{
-								pageSize === 10 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
-							}
-						</div>
-						<div className={pageSize === 25 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(25); togglePageSelector(); }}>
-							<span className={'text'}>25 PER PAGE</span>
-							{
-								pageSize === 25 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
-							}
-						</div>
-						<div className={pageSize === 50 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(50); togglePageSelector(); }}>
-							<span className={'text'}>50 PER PAGE</span>
-							{
-								pageSize === 50 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
-							}
-						</div>
-						<div className={pageSize === 100 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(100); togglePageSelector(); }}>
-							<span className={'text'}>100 PER PAGE</span>
-							{
-								pageSize === 100 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
-							}
-						</div>
-						<div className={pageSize === 0 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(1000); togglePageSelector(); }}>
-							<span className={'text'}>VIEW ALL</span>
-							{
-								pageSize === 0 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
-							}
-						</div>
-					</div>
-				) : (
-					<div role="button" tabIndex={0} onClick={togglePageSelector} className="closed">
-						{pageSize === 0 ? 'VIEW ALL' : `${pageSize} PER PAGE`}
-						<SvgWrapper width={'7px'} height={'13px'} fill={'#fff'} svgid={'pagination-arrows'} />
-					</div>
-				)
-			}
+			<div className={state ? 'open' : 'closed'}>
+				<div className={pageSize === 10 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(10); togglePageSelector(); }}>
+					<span className={'text'}>10 PER PAGE</span>
+					{
+						pageSize === 10 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
+					}
+				</div>
+				<div className={pageSize === 25 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(25); togglePageSelector(); }}>
+					<span className={'text'}>25 PER PAGE</span>
+					{
+						pageSize === 25 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
+					}
+				</div>
+				<div className={pageSize === 50 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(50); togglePageSelector(); }}>
+					<span className={'text'}>50 PER PAGE</span>
+					{
+						pageSize === 50 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
+					}
+				</div>
+				<div className={pageSize === 100 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(100); togglePageSelector(); }}>
+					<span className={'text'}>100 PER PAGE</span>
+					{
+						pageSize === 100 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
+					}
+				</div>
+				<div className={pageSize === 0 ? 'active' : 'inactive'} role="button" tabIndex={0} onClick={() => { setPageSize(1000); togglePageSelector(); }}>
+					<span className={'text'}>VIEW ALL</span>
+					{
+						pageSize === 0 ? <SvgWrapper width={'7px'} height={'13px'} svgid={'pagination-arrows'} /> : <span className={'svg-placeholder'}> </span>
+					}
+				</div>
+			</div>
+			<div role="button" tabIndex={0} onClick={togglePageSelector} className={state ? 'closed per-page-toggle' : 'per-page-toggle'}>
+				{pageSize === 0 ? 'VIEW ALL' : `${pageSize} PER PAGE`}
+			</div>
 		</div>
 	);
 }
