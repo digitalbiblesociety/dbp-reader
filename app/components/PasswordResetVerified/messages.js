e57bbd1552e38e8e2d76078fe1af13db4ee6469c
Jesse Hill <jsnahill@gmail.com>
2018-05-22 17:37:31 -0500

    Add component for where the user goes from the email to reset their password

---
+++ app/components/PasswordResetVerified/messages.js
@@ -0,0 +1,13 @@
+/*
+ * PasswordResetVerified Messages
+ *
+ * This contains all the text for the PasswordResetVerified component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+	header: {
+		id: 'app.components.PasswordResetVerified.header',
+		defaultMessage: 'This is the PasswordResetVerified component !',
+	},
+});
