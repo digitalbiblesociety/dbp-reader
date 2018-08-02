767fd872155d42db6f32b66a1a1398b53a9197db
Jesse Hill <jsnahill@gmail.com>
2018-08-02 13:43:17 -0500

    Update pm2 config to test spinning up multiple instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,17 +12,17 @@ module.exports = {
 				PORT: 3000,
 			},
 		},
-		{
-			name: 'bible.is.two',
-			script: './nextServer.js',
-			env: {
-				NODE_ENV: 'development',
-				PORT: 3001,
-			},
-			env_production: {
-				NODE_ENV: 'production',
-				PORT: 3001,
-			},
-		},
+		// {
+		// 	name: 'bible.is.two',
+		// 	script: './nextServer.js',
+		// 	env: {
+		// 		NODE_ENV: 'development',
+		// 		PORT: 3001,
+		// 	},
+		// 	env_production: {
+		// 		NODE_ENV: 'production',
+		// 		PORT: 3001,
+		// 	},
+		// },
 	],
 };
