7f2c715dc8712aa2dfe068e15890745716f0bef3
Jesse Hill <jsnahill@gmail.com>
2018-08-07 11:11:17 -0500

    Fix issue with the checkmark svg not loading, now using text checkmark with css instead

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
 module.exports = withSass(
 	withCss({
 		webpack(config) {
+			// config.module.rules.push({ test: /\.svg$/, loader: ['file-loader'] });
 			return commonsChunkConfig(config, /\.(sass|scss|css)$/);
 		},
 	}),
