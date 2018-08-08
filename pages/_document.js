08eddd50b338b237c4e2efdf635652b57ab3003f
Jesse Hill <jsnahill@gmail.com>
2018-08-07 19:06:28 -0500

    Add tag manager to next document

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,6 @@ export default class MyDocument extends Document {
 			<html>
 				<Head>
 					<link rel="stylesheet" href="/_next/static/style.css" />
-					{/*<link rel="stylesheet" href="../.next/static/style.css" />*/}
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<link rel="manifest" href="/static/manifest.json" />
 					<link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
@@ -71,10 +70,26 @@ export default class MyDocument extends Document {
 					/>
 					<meta property="fb:app_id" content="173528326559718" />
 					<script src="https://apis.google.com/js/platform.js" async defer />
+					<script
+						async
+						defer
+						dangerouslySetInnerHTML={{
+							__html:
+								'(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start": new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-N3RF6RC");',
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
 					<NextScript />
+					<noscript>
+						<iframe
+							src="https://www.googletagmanager.com/ns.html?id=GTM-N3RF6RC"
+							height="0"
+							width="0"
+							style={{ display: 'none', visibility: 'hidden' }}
+						/>
+					</noscript>
 				</body>
 			</html>
 		);
