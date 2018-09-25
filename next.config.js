fc78f304db003db2155b46b10f1b7a0fc9f0efa6
Jesse Hill <jsnahill@gmail.com>
2018-09-25 12:15:21 -0500

    Update nextjs config to create custom build_id

--- next.config.js
+++ next.config.js
@@ -1,12 +1,34 @@
 const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
+const childProcess = require('child_process');
+const promisify = require('util').promisify;
+const execPromise = promisify(childProcess.exec);
+const isProd = process.env.NODE_ENV === 'production';
 
 const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
-module.exports = withSass(
-	withCss({
-		webpack(config) {
-			// config.module.rules.push({ test: /\.svg$/, loader: ['file-loader'] });
-			return commonsChunkConfig(config, /\.(sass|scss|css)$/);
-		},
-	}),
-);
+if (isProd) {
+	module.exports = withSass(
+		withCss({
+			webpack(config) {
+				// config.module.rules.push({ test: /\.svg$/, loader: ['file-loader'] });
+				return commonsChunkConfig(config, /\.(sass|scss|css)$/);
+			},
+			generateBuildId: async () => {
+				let hash = '';
+				await execPromise('git rev-parse HEAD').then((res) => {
+					hash = res.stdout;
+				});
+				return hash;
+			},
+		}),
+	);
+} else {
+	module.exports = withSass(
+		withCss({
+			webpack(config) {
+				// config.module.rules.push({ test: /\.svg$/, loader: ['file-loader'] });
+				return commonsChunkConfig(config, /\.(sass|scss|css)$/);
+			},
+		}),
+	);
+}
