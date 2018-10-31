c17544d82b3f25cc713e7e5dad94eaedcaef6294
Jesse Hill <jsnahill@gmail.com>
2018-10-31 14:02:52 -0500

    Use build id env variable for production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
-const childProcess = require('child_process');
-const promisify = require('util').promisify;
-const execPromise = promisify(childProcess.exec);
+// const childProcess = require('child_process');
+// const promisify = require('util').promisify;
+// const execPromise = promisify(childProcess.exec);
 const isProd = process.env.NODE_ENV === 'production';
 
 // const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
@@ -10,11 +10,12 @@ if (isProd) {
 	module.exports = withSass(
 		withCss({
 			generateBuildId: async () => {
-				let hash = '';
-				await execPromise('git rev-parse HEAD').then((res) => {
-					hash = res.stdout;
-				});
-				return hash;
+				// let hash = '';
+				// await execPromise('git rev-parse HEAD').then((res) => {
+				// 	hash = res.stdout;
+				// });
+				// return hash;
+				return process.env.BUILD_ID;
 			},
 		}),
 	);
