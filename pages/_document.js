689366b6d57698a895e1ceb8e3eb313a0b3e381b
Jesse Hill <jsnahill@gmail.com>
2018-11-12 12:41:07 -0600

    Remove development tool reference

--- pages/_document.js
+++ pages/_document.js
@@ -66,9 +66,6 @@ export default class MyDocument extends Document {
 						/>
 					) : null}
 					<link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
-					{process.env.NODE_ENV === 'development' ? (
-						<script src={'http://localhost:8097'} />
-					) : null}
 					<meta
 						name="msvalidate.01"
 						content="ECDC0FFC6CCBA2520B05CD8B4F535D3C"
