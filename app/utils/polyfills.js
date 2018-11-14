2794b8330426c387d8fad38181bd1e987dfd1397
Jesse Hill <jsnahill@gmail.com>
2018-11-14 08:32:52 -0600

    Add polyfills for IE

--- app/utils/polyfills.js
+++ app/utils/polyfills.js
@@ -2,11 +2,19 @@
 
 import entries from 'core-js/fn/object/entries';
 import values from 'core-js/fn/object/values';
+import aIncludes from 'core-js/fn/array/includes';
+import includes from 'core-js/library/fn/string/includes';
 if (!Object.values) {
 	Object.values = values;
 }
 if (!Object.entries) {
 	Object.entries = entries;
+}
+if (!String.includes) {
+	String.includes = includes;
+}
+if (!Array.includes) {
+	Array.includes = aIncludes;
 }
 if (
 	typeof global !== 'undefined' &&
