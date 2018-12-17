7f7b17beff8963cf77cfdde5ae1932aec3718e67
Jesse Hill <jsnahill@gmail.com>
2018-12-17 13:35:31 -0600

    Add case to redux persist for catching facebook login

--- app/utils/stateReconciler.js
+++ app/utils/stateReconciler.js
@@ -1,12 +1,11 @@
 export default (incoming, original, reducedState) => {
-  const finalState = reducedState;
-  const incomingId = incoming.profile.userId;
-  const originalId = original.profile.userId;
-  const reducedId = reducedState.profile.userId;
+	const finalState = reducedState;
+	const incomingId = incoming.profile.userId;
+	const originalId = original.profile.userId;
 
-  if (originalId && !incomingId) {
-    finalState.profile.userId = originalId;
-  }
+	if (originalId && !incomingId) {
+		finalState.profile.userId = originalId;
+	}
 
-  return finalState;
+	return finalState;
 };
