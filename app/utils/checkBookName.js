4b30e58b2970ce31ce5d8d3f93ce0f1893ad7f2d
Jesse Hill <jsnahill@gmail.com>
2019-01-15 10:41:03 -0600

    Remove console logs

--- app/utils/checkBookName.js
+++ app/utils/checkBookName.js
@@ -3,9 +3,6 @@ const nameMap = require('./bookNameToId.json');
 
 module.exports = (inputId) => {
 	const lowercaseInput = inputId.toLowerCase();
-	console.log('inputId', inputId);
-	console.log('osisMap[lowercaseInput]', osisMap[lowercaseInput]);
-	console.log('nameMap[lowercaseInput]', nameMap[lowercaseInput]);
 	if (osisMap[lowercaseInput]) {
 		return osisMap[lowercaseInput];
 	} else if (nameMap[lowercaseInput]) {
