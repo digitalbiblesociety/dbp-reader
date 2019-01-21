7efa6c0d78fc3eb446c543282cb876f7f4ab9193
Jesse Hill <jsnahill@gmail.com>
2019-01-21 10:49:08 -0600

    Extract getFirstVerse and getLastVerse into util functions

---
+++ app/utils/requiresDom/getFirstSelectedVerse.js
@@ -0,0 +1,45 @@
+const getFirstVerse = ({
+	target,
+	button,
+	formattedSourceMain,
+	userSettings,
+	main,
+	getFormattedParentVerse,
+	getPlainParentVerseWithoutNumber,
+}) => {
+	const isFormatted =
+		!!formattedSourceMain &&
+		(!userSettings.getIn(['toggleOptions', 'readersMode', 'active']) ||
+			!userSettings.getIn(['toggleOptions', 'readersMode', 'available'])) &&
+		(!userSettings.getIn(['toggleOptions', 'oneVersePerLine', 'active']) ||
+			!userSettings.getIn(['toggleOptions', 'oneVersePerLine', 'available']));
+
+	try {
+		// if formatted iterate up the dom looking for data-id
+		if (isFormatted) {
+			const verseNode = getFormattedParentVerse(target);
+			const firstVerse = verseNode
+				? verseNode.attributes['data-id'].value.split('_')[1]
+				: '';
+
+			if (button && main.contains(target) && firstVerse) {
+				return firstVerse;
+			}
+		} else if (!isFormatted) {
+			const verseNode = getPlainParentVerseWithoutNumber(target);
+			const firstVerse = verseNode
+				? verseNode.attributes['data-verseid'].value
+				: '';
+
+			if (button && main.contains(target) && firstVerse) {
+				return firstVerse;
+			}
+		}
+	} catch (err) {
+		if (process.env.NODE_ENV === 'development') {
+			console.error('Error with getting last verse and opening menu', err); // eslint-disable-line no-console
+		}
+	}
+};
+
+export default getFirstVerse;
