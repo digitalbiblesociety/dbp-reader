3e53fd56e181f4770909c7ef7bab1dcc25ce240f
Jesse Hill <jsnahill@gmail.com>
2019-01-30 12:26:27 -0600

    Update tests for highlighting plain text.

--- app/containers/Text/tests/highlightPlain.test.js
+++ app/containers/Text/tests/highlightPlain.test.js
@@ -11,7 +11,7 @@ import highlightsObject from './sampleHighlights.json';
 *	13-14 & 14: multi-verse that ends up being two highlights in the same verse - false
 *	15 * 3: highlight overlaps multiple other highlights
 *	Tag to insert
-*	<em class="text-highlighted" style="background:linear-gradient(rgba(${sampleHighlights[0].highlighted_color}),rgba(${sampleHighlights[0].highlighted_color}))">
+*	<em class="text-highlighted" style="background:linear-gradient(${sampleHighlights[0].highlighted_color}),${sampleHighlights[0].highlighted_color}))">
 * */
 
 describe('highlightPlainText', () => {
@@ -33,7 +33,7 @@ describe('highlightPlainText', () => {
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 1: It should highlight the text for a single highlight in one verse', () => {
+	it('Case 1: It should highlight the text for a single highlight in one verse', () => {
 		const smallChapterData = chapterText.data;
 		const sampleHighlights = [highlightsObject.data[0]];
 
@@ -49,11 +49,11 @@ describe('highlightPlainText', () => {
 				hasHighlight: true,
 				verse_end: 1,
 				verse_end_alt: '1',
-				verse_text: `I <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `I <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">love</em> you, O LORD, my strength.`,
+				})">love</em> you, O LORD, my strength.`,
 			},
 		].concat(chapterText.data.slice(1));
 
@@ -61,7 +61,7 @@ describe('highlightPlainText', () => {
 			result,
 		);
 	});
-	xit('Case 2: It should handle a highlight that contains another highlight and use the color of the longer highlight', () => {
+	it('Case 2: It should handle a highlight that contains another highlight and use the color of the longer highlight', () => {
 		const sampleHighlights = highlightsObject.data.slice(1, 3);
 		const sampleText = chapterText.data.slice(1, 2);
 		const result = [
@@ -76,21 +76,21 @@ describe('highlightPlainText', () => {
 				verse_end: 2,
 				verse_end_alt: '2',
 				hasHighlight: true,
-				verse_text: `The<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `The<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))"> <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})"> <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">LORD</em> is my</em> rock and my fortress and my deliverer, my God, my rock, in whom I take refuge, my shield, and the horn of my salvation, my stronghold.`,
+				})">LORD</em> is my</em> rock and my fortress and my deliverer, my God, my rock, in whom I take refuge, my shield, and the horn of my salvation, my stronghold.`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 3: It should handle a multi-verse highlight', () => {
+	it('Case 3: It should handle a multi-verse highlight', () => {
 		const sampleHighlights = highlightsObject.data.slice(3, 4);
 		const sampleText = chapterText.data.slice(2, 4);
 		const result = [
@@ -105,11 +105,11 @@ describe('highlightPlainText', () => {
 				verse_end: 3,
 				verse_end_alt: '3',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">I call upon the LORD, who is worthy to be praised, and I am saved from my enemies.</em>`,
+				})">I call upon the LORD, who is worthy to be praised, and I am saved from my enemies.</em>`,
 			},
 			{
 				book_id: 'PSA',
@@ -122,17 +122,17 @@ describe('highlightPlainText', () => {
 				verse_end: 4,
 				verse_end_alt: '4',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">The cords of death encompassed me; the torrents of destruction assailed me</em>;`,
+				})">The cords of death encompassed me; the torrents of destruction assailed me</em>;`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 4: It should apply two non-overlapping highlights in the same verse', () => {
+	it('Case 4: It should apply two non-overlapping highlights in the same verse', () => {
 		const sampleHighlights = highlightsObject.data.slice(4, 6);
 		const sampleText = chapterText.data.slice(5, 6);
 		const result = [
@@ -147,21 +147,21 @@ describe('highlightPlainText', () => {
 				verse_end: 6,
 				verse_end_alt: '6',
 				hasHighlight: true,
-				verse_text: `In my <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `In my <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">distress</em> I called upon t<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">distress</em> I called upon t<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">he LO</em>RD; to my God I cried for help. From his temple he heard my voice, and my cry to him reached his ears.`,
+				})">he LO</em>RD; to my God I cried for help. From his temple he heard my voice, and my cry to him reached his ears.`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 5: It should stop the first highlight at the beginning of the next when they both are multi-verse and neither contains the other', () => {
+	it('Case 5: It should stop the first highlight at the beginning of the next when they both are multi-verse and neither contains the other', () => {
 		const sampleHighlights = highlightsObject.data.slice(6, 8);
 		const sampleText = chapterText.data.slice(7, 10);
 		const result = [
@@ -176,11 +176,11 @@ describe('highlightPlainText', () => {
 				verse_end: 8,
 				verse_end_alt: '8',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">Smoke went up from his nostrils, and devouring fire from his mouth; glowing coals flamed forth from him.</em>`,
+				})">Smoke went up from his nostrils, and devouring fire from his mouth; glowing coals flamed forth from him.</em>`,
 			},
 			{
 				book_id: 'PSA',
@@ -193,17 +193,17 @@ describe('highlightPlainText', () => {
 				verse_end: 9,
 				verse_end_alt: '9',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">He bowed the heavens and came down; thick darkness was under his </em><em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">He bowed the heavens and came down; thick darkness was under his </em><em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))"><em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})"><em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${sampleHighlights[0].highlighted_color}))">feet.</em></em>`,
+				},${sampleHighlights[0].highlighted_color})">feet.</em></em>`,
 			},
 			{
 				book_id: 'PSA',
@@ -216,17 +216,17 @@ describe('highlightPlainText', () => {
 				verse_end: 10,
 				verse_end_alt: '10',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">He rode on a cherub and flew; he came swiftly on the wings of the win</em>d.`,
+				})">He rode on a cherub and flew; he came swiftly on the wings of the win</em>d.`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 6: It should apply a multi-verse highlight correctly when there is a non-overlapping highlight in the ending verse', () => {
+	it('Case 6: It should apply a multi-verse highlight correctly when there is a non-overlapping highlight in the ending verse', () => {
 		const sampleHighlights = highlightsObject.data.slice(8, 10);
 		const sampleText = chapterText.data.slice(11, 13);
 		const result = [
@@ -241,11 +241,11 @@ describe('highlightPlainText', () => {
 				verse_end: 12,
 				verse_end_alt: '12',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">Out of the brightness before him hailstones and coals of fire broke through his clouds.</em>`,
+				})">Out of the brightness before him hailstones and coals of fire broke through his clouds.</em>`,
 			},
 			{
 				book_id: 'PSA',
@@ -258,21 +258,21 @@ describe('highlightPlainText', () => {
 				verse_end: 13,
 				verse_end_alt: '13',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">The LORD also thundered</em> in the heavens, and <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">The LORD also thundered</em> in the heavens, and <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">the Most </em>High uttered his voice, hailstones and coals of fire.`,
+				})">the Most </em>High uttered his voice, hailstones and coals of fire.`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 7: It should apply two overlapping highlights where newest highlight take priority', () => {
+	it('Case 7: It should apply two overlapping highlights where newest highlight take priority', () => {
 		const sampleHighlights = [
 			{
 				id: 262,
@@ -308,21 +308,21 @@ describe('highlightPlainText', () => {
 				verse_end: 12,
 				verse_end_alt: '12',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">Out of the <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">Out of the <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">brightness</em></em> before him hailstones and coals of fire broke through his clouds.`,
+				})">brightness</em></em> before him hailstones and coals of fire broke through his clouds.`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 8: It should apply a newer highlight over the top of two pre-existing highlights where the new highlight starts at the same index as the first one and ends at the same index of the second', () => {
+	it('Case 8: It should apply a newer highlight over the top of two pre-existing highlights where the new highlight starts at the same index as the first one and ends at the same index of the second', () => {
 		const sampleHighlights = [
 			{
 				id: 262,
@@ -368,26 +368,26 @@ describe('highlightPlainText', () => {
 				verse_end: 12,
 				verse_end_alt: '12',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[2].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[2].highlighted_color
-				}))"><em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})"><em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">Out</em> of the <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">Out</em> of the <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">brightness</em></em> before him hailstones and coals of fire broke through his clouds.`,
+				})">brightness</em></em> before him hailstones and coals of fire broke through his clouds.`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 		// expect(false).toBe(true);
 	});
-	xit('Case 9: It should apply a highlight contained within an older highlight where the new highlight is on top of the old one and they are the same color', () => {
+	it('Case 9: It should apply a highlight contained within an older highlight where the new highlight is on top of the old one and they are the same color', () => {
 		const sampleHighlights = [
 			{
 				bible_id: 'ENGESV',
@@ -439,21 +439,21 @@ describe('highlightPlainText', () => {
 				verse_end: 1,
 				verse_end_alt: '1',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">The <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">The <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">book of the genealogy of Jesus</em> Christ</em>, the son of David, the son of Abraham.`,
+				})">book of the genealogy of Jesus</em> Christ</em>, the son of David, the son of Abraham.`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 	});
-	xit('Case 10: It should apply a highlight contained within an older highlight where the new highlight is on top of the old one and they are NOT the same color', () => {
+	it('Case 10: It should apply a highlight contained within an older highlight where the new highlight is on top of the old one and they are NOT the same color', () => {
 		const sampleHighlights = [
 			{
 				bible_id: 'ENGESV',
@@ -505,15 +505,15 @@ describe('highlightPlainText', () => {
 				verse_end: 1,
 				verse_end_alt: '1',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))">The <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">The <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">book of the genealogy of Jesus</em> Christ</em>, the son of David, the son of Abraham.`,
+				})">book of the genealogy of Jesus</em> Christ</em>, the son of David, the son of Abraham.`,
 			},
 		];
 		const sampleHighlights2 = [
@@ -567,22 +567,22 @@ describe('highlightPlainText', () => {
 				verse_end: 13,
 				verse_end_alt: '13',
 				hasHighlight: true,
-				verse_text: `and <em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `and <em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights2[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights2[0].highlighted_color
-				}))"><em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})"><em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights2[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights2[1].highlighted_color
-				}))">Zerubbabel</em> the father of Abiud</em>, and Abiud the father of Eliakim, and Eliakim the father of Azor,`,
+				})">Zerubbabel</em> the father of Abiud</em>, and Abiud the father of Eliakim, and Eliakim the father of Azor,`,
 			},
 		];
 
 		expect(highlightPlainText(sampleHighlights, sampleText)).toEqual(result);
 		expect(highlightPlainText(sampleHighlights2, sampleText2)).toEqual(result2);
 	});
-	xit('Case 11: It should apply a newer highlight over the top of two pre-existing highlights where the new highlight does not start at the same index as the first one and does not end at the same index of the second', () => {
+	it('Case 11: It should apply a newer highlight over the top of two pre-existing highlights where the new highlight does not start at the same index as the first one and does not end at the same index of the second', () => {
 		const sampleHighlights = [
 			{
 				id: 262,
@@ -628,19 +628,19 @@ describe('highlightPlainText', () => {
 				verse_end: 12,
 				verse_end_alt: '12',
 				hasHighlight: true,
-				verse_text: `<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				verse_text: `<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[2].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[2].highlighted_color
-				}))">Out<em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})">Out<em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[0].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[0].highlighted_color
-				}))"> of the </em><em class="text-highlighted" style="background:linear-gradient(rgba(${
+				})"> of the </em><em class="text-highlighted" style="background:linear-gradient(${
 					sampleHighlights[1].highlighted_color
-				}),rgba(${
+				},${
 					sampleHighlights[1].highlighted_color
-				}))">brightness</em> before</em> him hailstones and coals of fire broke through his clouds.`,
+				})">brightness</em> before</em> him hailstones and coals of fire broke through his clouds.`,
 			},
 		];
 
