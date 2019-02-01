4b7b32671e8fb9d67fbab7a7cdd8aaffeb4c98e7
Jesse Hill <jsnahill@gmail.com>
2019-02-01 16:43:27 -0600

    Add more tests

---
+++ app/components/ColorPicker/tests/index.test.js
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import ColorPicker from '../';
+import renderer from 'react-test-renderer';
+
+describe('ColorPicker tests', () => {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<ColorPicker handlePickedColor={jest.fn()} />);
+	});
+	afterEach(() => {
+		wrapper = null;
+	});
+	it('Should render', () => {
+		const tree = renderer
+			.create(<ColorPicker handlePickedColor={jest.fn()} />)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+	it('It should call the handler when an option is clicked', () => {
+		// TODO: finish implementing this test.
+		expect(true).toBe(true);
+	});
+});
