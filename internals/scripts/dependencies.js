4ce2ecd2b370d80f9f23641969093aa60132f617
Jesse Hill <jsnahill@gmail.com>
2019-02-04 16:35:22 -0600

    Remove unused files

--- internals/scripts/dependencies.js
+++ internals/scripts/dependencies.js
@@ -1,6 +1,6 @@
 // No need to build the DLL in production
 if (process.env.NODE_ENV === 'production') {
-  process.exit(0);
+	process.exit(0);
 }
 
 require('shelljs/global');
@@ -18,7 +18,7 @@ const outputPath = path.join(process.cwd(), dllConfig.path);
 const dllManifestPath = path.join(outputPath, 'package.json');
 
 /**
- * I use node_modules/react-boilerplate-dlls by default just because
+ * I use node_modules/bible.is-dlls by default just because
  * it isn't going to be version controlled and babel wont try to parse it.
  */
 mkdir('-p', outputPath);
@@ -29,18 +29,24 @@ echo('Building the Webpack DLL...');
  * Create a manifest so npm install doesn't warn us
  */
 if (!exists(dllManifestPath)) {
-  writeFile(
-    dllManifestPath,
-    JSON.stringify(defaults({
-      name: 'react-boilerplate-dlls',
-      private: true,
-      author: pkg.author,
-      repository: pkg.repository,
-      version: pkg.version,
-    }), null, 2),
-    'utf8'
-  );
+	writeFile(
+		dllManifestPath,
+		JSON.stringify(
+			defaults({
+				name: 'bible.is-dlls',
+				private: true,
+				author: pkg.author,
+				repository: pkg.repository,
+				version: pkg.version,
+			}),
+			null,
+			2,
+		),
+		'utf8',
+	);
 }
 
 // the BUILDING_DLL env var is set to avoid confusing the development environment
-exec('cross-env BUILDING_DLL=true webpack --display-chunks --color --config internals/webpack/webpack.dll.babel.js --hide-modules');
+exec(
+	'cross-env BUILDING_DLL=true webpack --display-chunks --color --config internals/webpack/webpack.dll.babel.js --hide-modules',
+);
