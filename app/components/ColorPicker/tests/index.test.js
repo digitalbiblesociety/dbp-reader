f8243c9b14630330afaf884fec4ef97aaf3b373a
Jesse Hill <jsnahill@gmail.com>
2019-02-14 10:52:32 -0600

    Add tests for color picker component

--- app/components/ColorPicker/tests/index.test.js
+++ app/components/ColorPicker/tests/index.test.js
@@ -1,24 +1,40 @@
-// import { shallow } from 'enzyme';
 import React from 'react';
+import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import ColorPicker from '..';
+
+const colorToMatch = 'rgba(84,185,72,.5)';
+const handlePickedColor = jest.fn(({ color }) => color);
 
 describe('ColorPicker tests', () => {
 	// let wrapper;
 	// beforeEach(() => {
-	//   wrapper = shallow(<ColorPicker handlePickedColor={jest.fn()} />);
+	//   wrapper = mount(<ColorPicker handlePickedColor={jest.fn()} />);
 	// });
 	// afterEach(() => {
 	//   wrapper = null;
 	// });
 	it('Should render', () => {
 		const tree = renderer
-			.create(<ColorPicker handlePickedColor={jest.fn()} />)
+			.create(<ColorPicker handlePickedColor={handlePickedColor} />)
 			.toJSON();
 		expect(tree).toMatchSnapshot();
 	});
 	it('It should call the handler when an option is clicked', () => {
 		// TODO: finish implementing this test.
-		expect(true).toBe(true);
+		const wrapper = mount(
+			<ColorPicker handlePickedColor={handlePickedColor} />,
+		);
+		const colorNode = wrapper.find('.green');
+		const spy = jest.spyOn(colorNode.props(), 'onClick');
+		const colorOutput = colorNode.props().onClick();
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(colorOutput).toEqual(colorToMatch);
+
+		colorNode.simulate('click');
+
+		expect(handlePickedColor).toHaveBeenCalledTimes(2);
+		expect(wrapper).toBeTruthy();
 	});
 });
