14e92499883063c5a2a9108c524a278a394d6d10
Jesse Hill <jsnahill@gmail.com>
2019-02-15 08:53:06 -0600

    Add tests for HighlightColors

--- app/components/HighlightColors/tests/index.test.js
+++ app/components/HighlightColors/tests/index.test.js
@@ -9,7 +9,7 @@ jest.mock('react-intl', () => ({
 	defineMessages: (messages) => messages,
 }));
 /* eslint-enable react/prop-types */
-
+const options = ['None', 'Yellow', 'Green', 'Pink', 'Purple', 'Blue'];
 const green = 'rgba(84,185,72,.5)';
 const yellow = 'rgba(252,230,0,.5)';
 const pink = 'rgba(208,105,169,.5)';
@@ -20,8 +20,8 @@ let wrapper;
 
 describe('HighlightColors component', () => {
 	beforeEach(() => {
-		wrapper = mount(<HighlightColors addHighlight={addHighlight} />);
 		addHighlight = jest.fn(({ color }) => color);
+		wrapper = mount(<HighlightColors addHighlight={addHighlight} />);
 	});
 	it('should match snapshot', () => {
 		const tree = renderer.create(
@@ -30,38 +30,80 @@ describe('HighlightColors component', () => {
 		expect(tree).toMatchSnapshot();
 	});
 	it('should contain yellow highlight option', () => {
-		const colorSpan = wrapper.find('.yellow');
-		colorSpan.simulate('click');
+		const colorNode = wrapper.find('.yellow');
+		const spy = jest.spyOn(colorNode.props(), 'onClick');
+		const colorOutput = colorNode.props().onClick({ clientX: 10, clientY: 10 });
 
-		expect(addHighlight).toHaveBeenCalledTimes(1);
-		expect(colorSpan.text()).toEqual('yellow');
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(colorOutput).toEqual(yellow);
+
+		colorNode.simulate('click');
+
+		expect(addHighlight).toHaveBeenCalledTimes(2);
 	});
 	it('should contain green highlight option', () => {
-		const colorSpan = wrapper.find('.green');
-		colorSpan.simulate('click');
+		const colorNode = wrapper.find('.green');
+		const spy = jest.spyOn(colorNode.props(), 'onClick');
+		const colorOutput = colorNode.props().onClick({ clientX: 10, clientY: 10 });
 
-		expect(addHighlight).toHaveBeenCalledTimes(1);
-		expect(colorSpan.text()).toEqual('green');
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(colorOutput).toEqual(green);
+
+		colorNode.simulate('click');
+
+		expect(addHighlight).toHaveBeenCalledTimes(2);
 	});
 	it('should contain pink highlight option', () => {
-		const colorSpan = wrapper.find('.pink');
-		colorSpan.simulate('click');
+		const colorNode = wrapper.find('.pink');
+		const spy = jest.spyOn(colorNode.props(), 'onClick');
+		const colorOutput = colorNode.props().onClick({ clientX: 10, clientY: 10 });
 
-		expect(addHighlight).toHaveBeenCalledTimes(1);
-		expect(colorSpan.text()).toEqual('pink');
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(colorOutput).toEqual(pink);
+
+		colorNode.simulate('click');
+
+		expect(addHighlight).toHaveBeenCalledTimes(2);
 	});
 	it('should contain purple highlight option', () => {
-		const colorSpan = wrapper.find('.purple');
-		colorSpan.simulate('click');
+		const colorNode = wrapper.find('.purple');
+		const spy = jest.spyOn(colorNode.props(), 'onClick');
+		const colorOutput = colorNode.props().onClick({ clientX: 10, clientY: 10 });
 
-		expect(addHighlight).toHaveBeenCalledTimes(1);
-		expect(colorSpan.text()).toEqual('purple');
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(colorOutput).toEqual(purple);
+
+		colorNode.simulate('click');
+
+		expect(addHighlight).toHaveBeenCalledTimes(2);
 	});
 	it('should contain blue highlight option', () => {
-		const colorSpan = wrapper.find('.blue');
-		colorSpan.simulate('click');
+		const colorNode = wrapper.find('.blue');
+		const spy = jest.spyOn(colorNode.props(), 'onClick');
+		const colorOutput = colorNode.props().onClick({ clientX: 10, clientY: 10 });
 
-		expect(addHighlight).toHaveBeenCalledTimes(1);
-		expect(colorSpan.text()).toEqual('blue');
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(colorOutput).toEqual(blue);
+
+		colorNode.simulate('click');
+
+		expect(addHighlight).toHaveBeenCalledTimes(2);
+	});
+	it('should contain blue highlight option', () => {
+		const colorNode = wrapper.find('.none');
+		const spy = jest.spyOn(colorNode.props(), 'onClick');
+		const colorOutput = colorNode.props().onClick({ clientX: 10, clientY: 10 });
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(colorOutput).toEqual('none');
+
+		colorNode.simulate('click');
+
+		expect(addHighlight).toHaveBeenCalledTimes(2);
+	});
+	it('should contain all the color options', () => {
+		const nodes = wrapper.find('.color-text');
+
+		nodes.forEach((node, index) => expect(node.text()).toEqual(options[index]));
 	});
 });
