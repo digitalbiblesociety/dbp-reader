a6abc4d93d223f580888bb1bb3d71c37a6c88a53
Jesse Hill <jsnahill@gmail.com>
2019-02-20 12:47:45 -0600

    Update pm2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,43 @@
 module.exports = {
-	apps: [
-		{
-			name: 'bible.is.prod',
-			script: './nextServer.js',
-			instances: 1,
-			env: {
-				NODE_ENV: 'production',
-				PORT: 3000,
-			},
-			env_production: {
-				NODE_ENV: 'production',
-				PORT: 3000,
-			},
-		},
-		{
-			name: 'bible.is.dev',
-			script: './nextServer.js',
-			instances: 1,
-			env: {
-				NODE_ENV: 'development',
-				PORT: 3001,
-			},
-			env_production: {
-				NODE_ENV: 'production',
-				PORT: 3000,
-			},
-		},
-	],
+  apps: [
+    {
+      name: 'bible.is.prod',
+      script: './nextServer.js',
+      instances: 1,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+    {
+      name: 'bible.is.dev',
+      script: './nextServer.js',
+      instances: 1,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3001,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+    {
+      name: 'bible.is.stage',
+      script: './nextServer.js',
+      instances: 1,
+      env: {
+        NODE_ENV: 'staging',
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'staging',
+        PORT: 3000,
+      },
+    },
+  ],
 };
