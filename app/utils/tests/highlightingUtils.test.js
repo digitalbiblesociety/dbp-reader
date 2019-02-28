4b52a21f827fd1aa6fe053e24496ea25df455c0b
Jesse Hill <jsnahill@gmail.com>
2019-02-28 14:48:18 -0600

    Add more tests for highlighting utils

--- app/utils/tests/highlightingUtils.test.js
+++ app/utils/tests/highlightingUtils.test.js
@@ -8,4 +8,11 @@ describe('Highlighting utils methods', () => {
       expect(replacedString).toEqual('aaaaaa');
     });
   });
+  describe('getReference', () => {
+    it('should generate a reference off the given parameters', () => {
+      const reference = utils.getReference(1, 3, 'Genesis', 5);
+
+      expect(reference).toEqual('Genesis 5:1-3');
+    });
+  });
 });
