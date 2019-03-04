226ba5ece0fa79e83320d5b44db7049c6b554831
Jesse Hill <jsnahill@gmail.com>
2019-03-04 13:31:24 -0600

    Add more tests to audio calls

---
+++ app/utils/testUtils/filesetsForAudioCall.js
@@ -0,0 +1,132 @@
+export const engesv = [
+  {
+    id: 'ENGESVN2DA',
+    type: 'audio_drama',
+    size: 'NT',
+  },
+  {
+    id: 'ENGESV',
+    type: 'text_plain',
+    size: 'C',
+  },
+  {
+    id: 'ENGGIDO1DA',
+    type: 'audio',
+    size: 'OT',
+  },
+  {
+    id: 'ENGESVC2DA16',
+    type: 'audio_drama',
+    size: 'P',
+  },
+  {
+    id: 'ENGGIDN2DA',
+    type: 'audio_drama',
+    size: 'NT',
+  },
+  {
+    id: 'ENGESVC2DA',
+    type: 'audio_drama',
+    size: 'OT',
+  },
+  {
+    id: 'ENGESVO2DA',
+    type: 'audio_drama',
+    size: 'OT',
+  },
+  {
+    id: 'ENGGIDN1DA',
+    type: 'audio',
+    size: 'NT',
+  },
+  {
+    id: 'ENGESVO1DA',
+    type: 'audio',
+    size: 'OT',
+  },
+  {
+    id: 'ENGGIDO2DA',
+    type: 'audio_drama',
+    size: 'OT',
+  },
+  {
+    id: 'ENGESV',
+    type: 'text_format',
+    size: 'C',
+  },
+  {
+    id: 'ENGESVN1DA',
+    type: 'audio',
+    size: 'NT',
+  },
+  {
+    id: 'ENGGID',
+    type: 'text_plain',
+    size: 'C',
+  },
+];
+export const ntpotp = [
+  {
+    id: 'OREWBTP1DA',
+    type: 'audio',
+    size: 'OTP',
+  },
+  {
+    id: 'OREWBTS1DA',
+    type: 'audio',
+    size: 'OTP',
+  },
+  {
+    id: 'OREWBTP2DA',
+    type: 'audio_drama',
+    size: 'NTP',
+  },
+];
+export const seawbt = [
+  {
+    id: 'SEAWYIP2DA',
+    type: 'audio_drama',
+    size: 'NTPOTP',
+  },
+  {
+    id: 'SEAWYIP2DA',
+    type: 'app',
+    size: 'NTP',
+  },
+  {
+    id: 'SEAWYIP1DA',
+    type: 'audio',
+    size: 'NTPOTP',
+  },
+  {
+    id: 'SEAWYIP2DA16',
+    type: 'audio_drama',
+    size: 'NTPOTP',
+  },
+];
+export const filesets3 = [
+  { id: 'ENGESV', type: 'text_plain', size: 'C' },
+  { id: 'ENGESVO2DA', type: 'audio_drama', size: 'OT' },
+  { id: 'ENGESVO1DA', type: 'audio', size: 'OT' },
+  { id: 'ENGESV', type: 'text_format', size: 'C' },
+];
+export const filesets4 = [
+  { id: 'ENGESVN2DA', type: 'audio_drama', size: 'NT' },
+  { id: 'ENGESVN1DA', type: 'audio', size: 'NT' },
+];
+export const filesets5 = [
+  { id: 'ENGESVO2DA', type: 'audio_drama', size: 'OT' },
+  { id: 'ENGESVO1DA', type: 'audio', size: 'OT' },
+];
+export const filesets6 = [
+  { id: 'ENGESV', type: 'text_plain', size: 'C' },
+  { id: 'ENGESV', type: 'text_format', size: 'C' },
+];
+export const filesets7 = [
+  { id: 'ENGESV', type: 'text_plain', size: 'C' },
+  { id: 'ENGESV', type: 'text_format', size: 'C' },
+];
+export const filesets8 = [
+  { id: 'ENGESV', type: 'text_plain', size: 'OT' },
+  { id: 'ENGESV', type: 'text_format', size: 'OT' },
+];
