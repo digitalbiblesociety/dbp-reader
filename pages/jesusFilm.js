e979d3fdadb0ac1cab2e68704b99e7c7bf41e2e8
Jesse Hill <jsnahill@gmail.com>
2019-03-26 13:47:09 -0500

    Add navbar to top of Jesus Film page

--- pages/jesusFilm.js
+++ pages/jesusFilm.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import request from '../app/utils/request';
+import isUserAgentInternetExplorer from '../app/utils/isUserAgentInternetExplorer';
+import parseCookie from '../app/utils/parseCookie';
 
 // Basic nav
 // Basic footer
 // Video Player with adjusted styles
-const JesusFilm = ({ iso, routeLocation, hlsStream }) => {
-  const titleText = `Jesus Film ${iso} | Bible.is`;
+const JesusFilm = ({ iso, routeLocation, hlsStream, theme, isIe }) => {
+  const titleText = `Jesus Film | ${iso} | Bible.is`;
 
   return (
     <div>
@@ -29,20 +31,69 @@ const JesusFilm = ({ iso, routeLocation, hlsStream }) => {
         <meta name={'twitter:description'} content={titleText} />
         <title>{titleText}</title>
       </Head>
+      <div id={'navigation-bar'} className={'nav-background'}>
+        <div className={'nav-container'}>
+          <a
+            className={'logo'}
+            href={'http://www.bible.is'}
+            title={'http://www.bible.is'}
+            rel={'noopener'}
+          >
+            {theme === 'paper' &&
+              !isIe && (
+                <svg className={'svg'}>
+                  <use
+                    xlinkHref={
+                      '/static/light_theme_logo.svg#bible.is_logo_light'
+                    }
+                  />
+                </svg>
+              )}
+            {(theme !== 'paper' || isIe) && (
+              <svg className={'svg'} fill={isIe ? '#fff' : ''}>
+                <use xlinkHref={'/static/dark_theme_logo.svg#bible.is_logo'} />
+              </svg>
+            )}
+          </a>
+        </div>
+      </div>
       <span style={{ color: 'black' }}>
         This is the Jesus Film Page with ISO: {iso} and location:{' '}
         {routeLocation}
       </span>
       <pre />
       <code>{hlsStream}</code>
-      <div className="footer-background" />
+      <div className={'footer-background'} />
     </div>
   );
 };
 
 JesusFilm.getInitialProps = async (context) => {
+  const { req } = context;
   const routeLocation = context.asPath;
   const { iso } = context.query;
+  const hasHeaders = req && req.headers;
+
+  // Default isIe to false
+  let isIe = false;
+  // Default theme name is red
+  let theme = 'red';
+
+  // Check user agent for ie and get current theme
+  if (hasHeaders) {
+    isIe = isUserAgentInternetExplorer(req.headers['user-agent']);
+  } else {
+    isIe = isUserAgentInternetExplorer(navigator.userAgent);
+  }
+
+  // Get current theme
+  if (hasHeaders && req.headers.cookie) {
+    theme = parseCookie(req.headers.cookie).bible_is_theme || 'red';
+  } else if (typeof document !== 'undefined' && document.cookie) {
+    theme = parseCookie(document.cookie).bible_is_theme || 'red';
+  }
+
+  // Data fetching
   const jfResponse = await request(
     `${process.env.BASE_API_ROUTE}/arclight/jesus-film/languages?key=${
       process.env.DBP_API_KEY
@@ -52,20 +103,24 @@ JesusFilm.getInitialProps = async (context) => {
   const hlsStream = arclightId
     ? `${process.env.BASE_API_ROUTE}/arclight/jesus-film?key=${
         process.env.DBP_API_KEY
-      }&v=4&arclightId=${arclightId}`
+      }&v=4&arclight_id=${arclightId}`
     : '';
 
   return {
     routeLocation,
     iso,
     hlsStream,
+    theme,
+    isIe,
   };
 };
 
 JesusFilm.propTypes = {
   iso: PropTypes.string,
-  routeLocation: PropTypes.string,
+  theme: PropTypes.string,
   hlsStream: PropTypes.string,
+  routeLocation: PropTypes.string,
+  isIe: PropTypes.bool,
 };
 
 export default JesusFilm;
