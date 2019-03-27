d3c93c77f6b211bba4865b4ae83bb805f46fd904
Jesse Hill <jsnahill@gmail.com>
2019-03-27 13:54:06 -0500

    Fix issue with Jesus Film duration times

--- app/utils/getFormattedTimeString.js
+++ app/utils/getFormattedTimeString.js
@@ -1,26 +1,26 @@
 // Time is in seconds
 export default function getFormattedTimeString(time) {
-  // Calculate values from number of milliseconds
-  const durSecs = Math.floor(time % 60);
-  const durMins = Math.floor((time / 60) % 60);
-  const durHours = Math.floor(time / 60 / 60 / 60);
+	// Calculate values from number of milliseconds
+	const durSecs = Math.floor(time % 60);
+	const durMins = Math.floor((time / 60) % 60);
+	const durHours = Math.floor(time / 60 / 60);
 
-  // Build strings
-  const durSecsString =
-    durSecs.toFixed(0).length === 1
-      ? `0${durSecs.toFixed(0)}`
-      : durSecs.toFixed(0);
-  const durMinsString =
-    durMins.toFixed(0).length === 1
-      ? `0${durMins.toFixed(0)}`
-      : durMins.toFixed(0);
-  const durHoursString =
-    durHours.toFixed(0).length === 1
-      ? `0${durHours.toFixed(0)}`
-      : durHours.toFixed(0);
+	// Build strings
+	const durSecsString =
+		durSecs.toFixed(0).length === 1
+			? `0${durSecs.toFixed(0)}`
+			: durSecs.toFixed(0);
+	const durMinsString =
+		durMins.toFixed(0).length === 1
+			? `0${durMins.toFixed(0)}`
+			: durMins.toFixed(0);
+	const durHoursString =
+		durHours.toFixed(0).length === 1
+			? `0${durHours.toFixed(0)}`
+			: durHours.toFixed(0);
 
-  if (durHoursString !== '00') {
-    return `${durHoursString}:${durMinsString}:${durSecsString}`;
-  }
-  return `${durMinsString}:${durSecsString}`;
+	if (durHoursString !== '00') {
+		return `${durHoursString}:${durMinsString}:${durSecsString}`;
+	}
+	return `${durMinsString}:${durSecsString}`;
 }
