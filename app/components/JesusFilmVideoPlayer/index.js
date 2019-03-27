a2ef7ecdda2168c98046782f1e6f6a0a50c9cd33
Jesse Hill <jsnahill@gmail.com>
2019-03-27 09:08:46 -0500

    Create Jesus Film Video Player

---
+++ app/components/JesusFilmVideoPlayer/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* JesusFilmVideoPlayer
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class JesusFilmVideoPlayer extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+	render() {
+		return (
+			<div>
+			</div>
+		);
+	}
+}
+
+JesusFilmVideoPlayer.propTypes = {
+
+};
+
+export default JesusFilmVideoPlayer;
---
+++ app/components/JesusFilmVideoPlayer/tests/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import JesusFilmVideoPlayer from '..';
+
+describe('
+<JesusFilmVideoPlayer /> component tests', () => {
+it('Should match snapshot with default props', () => {
+const tree = renderer.create(
+<JesusFilmVideoPlayer />).toJSON();
+
+expect(tree).toMatchSnapshot();
+});
+});
