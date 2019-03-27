1f803fb93b48cfa789f82382fea100eda88826c2
Jesse Hill <jsnahill@gmail.com>
2019-03-27 10:47:05 -0500

    Fix jesus film video player tests

--- app/components/JesusFilmVideoPlayer/tests/index.test.js
+++ app/components/JesusFilmVideoPlayer/tests/index.test.js
@@ -3,12 +3,10 @@ import renderer from 'react-test-renderer';
 
 import JesusFilmVideoPlayer from '..';
 
-describe('
-<JesusFilmVideoPlayer /> component tests', () => {
-it('Should match snapshot with default props', () => {
-const tree = renderer.create(
-<JesusFilmVideoPlayer />).toJSON();
+describe('<JesusFilmVideoPlayer /> component tests', () => {
+	it('Should match snapshot with default props', () => {
+		const tree = renderer.create(<JesusFilmVideoPlayer />).toJSON();
 
-expect(tree).toMatchSnapshot();
-});
+		expect(tree).toMatchSnapshot();
+	});
 });
