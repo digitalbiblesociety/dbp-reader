16f51a13783dca8dd1971529b360b53b46deb41b
Jesse Hill <jesse_hill@eogresources.com>
2019-10-09 16:30:16 -0500

    Add enquirer for prompts, get typescript to compile successfully

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,5 +1,24 @@
 const fs = require("fs");
+const { prompt } = require("enquirer");
 
-module.exports = async () => {
+const app = async () => {
   console.log("App is running now!");
+  const { username, password } = await prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your username?"
+    },
+    {
+      type: "password",
+      name: "password",
+      message: "What is your password?"
+    }
+  ]);
+  console.log("username", username);
+  console.log("password", password);
 };
+
+app();
+
+module.exports = app;
